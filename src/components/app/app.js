70de2bac5abe9fcfb1e61de1b1f7decaf1e96f0d
Aleksandr Zhuravlev <ja@idaproject.com>
2021-01-04 14:33:01 +0300

    добавил HOC компоненты

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,98 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Header from "../header";
 import RandomPlanet from "../random-planet";
-import ErrorButton from "../error-button";
-import ErrorIndicator from "../error-indicator";
-import PeoplePage from "../people-page";
-// import ItemList from "../item-list";
-// import PersonDetails from "../person-details";
+import ErrorBoundry from "../error-boundry";
+
 import SwapiService from "../../services/swapi-service";
+
+import {
+    PersonDetails,
+    PlanetDetails,
+    StarshipDetails,
+    PersonList,
+    PlanetList,
+    StarshipList
+  } from '../sw-components';
 
 import "./app.css";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+export default class App extends Component {
+  swapiService = new SwapiService();
 
-    this.state = {
-      showRandomPlanet: true,
-      hasError: false,
-    };
+  state = {
+    showRandomPlanet: true,
+  };
 
-    this.swapiService = new SwapiService();
-
-    this.toggleRandomPlanet = this.toggleRandomPlanet.bind(this);
-  }
-
-  componentDidCatch() {
-    this.setState({ hasError: true });
-  }
-
-  toggleRandomPlanet() {
+  toggleRandomPlanet = () => {
     this.setState((state) => {
       return {
         showRandomPlanet: !state.showRandomPlanet,
       };
     });
-  }
-
-  onPersonSelected(id) {
-    this.setState({
-      selectedPerson: id,
-    });
-  }
+  };
 
   render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator />;
-    }
-
     const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 
     return (
-      <div className="stardb-app">
-        <Header />
-        {planet}
+      <ErrorBoundry>
+        <div className="stardb-app">
+          <Header />
 
-        <div className="row mb2 button-row">
-          <button
-            className="toggle-planet btn btn-warning btn-lg"
-            onClick={this.toggleRandomPlanet}
-          >
-            Toggle Random Planet
-          </button>
-          <ErrorButton />
+          <PersonDetails itemId={11} />
+
+          <PlanetDetails itemId={5} />
+
+          <StarshipDetails itemId={9} />
+
+          <PersonList>
+            {({ name }) => <span>{name}</span>}
+          </PersonList>
+
+          <PlanetList>
+            {({ name }) => <span>{name}</span>}
+          </PlanetList>
+
+          <StarshipList>
+            {({ name }) => <span>{name}</span>}
+          </StarshipList>
         </div>
-
-        <PeoplePage />
-
-        {/* <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllPlanets}
-              renderItem={(item) => item.name}
-            />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllStarships}
-              renderItem={(item) => item.name}
-            />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div> */}
-      </div>
+      </ErrorBoundry>
     );
   }
 }
